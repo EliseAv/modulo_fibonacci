7dfa10356461367f2402e11826ce6097ac3661ae
Ekevoo <x@ekevoo.com>
2019-01-03 13:54:35 -0500

    added a better alphabet

--- modfibo.py
+++ modfibo.py
@@ -29,18 +29,50 @@ home I wrote this little test app to explore bigger bases. Base 11 was really ha
 There is no purpose to this. It's just fun with numbers. Either you get it or you're not a nerd, and either way is fine.
 
 """
-from collections import defaultdict
-from sys import argv, stdout
+import collections
+import collections.abc
+import itertools
+import math
+import sys
+from typing import Iterable, Dict, List, Sequence, Tuple
 
-ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_/+=!?@#$%^&*;,.:"\'\\`~(){}<>|'
+import colorama
+
+
+class Alphabet:
+    # 82-symbol base alphabet so that 2-digit representations will use 0-9 at minimum
+    ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&?!|/*^+=~-_;:,.'
+
+    def __init__(self, base: int):
+        colorama.init()
+        self.length = math.ceil(math.log(base, len(self.ALPHABET)))
+        symbols = sub_alphabet = self.ALPHABET[:math.ceil(base ** (1 / self.length))]
+        # increment the length of each symbol until we reach the target length
+        for _ in range(1, self.length):
+            symbols = [ii + i for ii in symbols for i in sub_alphabet]
+        self.symbols: Sequence[str] = symbols[:base]
+
+    def dump(self, runs: Iterable[List[int]]) -> None:
+        for run in runs:
+            output = [colorama.Style.BRIGHT, colorama.Fore.MAGENTA, self.symbols[run[-1]]]
+            if self.length == 1:
+                output.append(colorama.Fore.GREEN)
+                output.extend(self.symbols[i] for i in run)
+            else:
+                zebra = (colorama.Fore.GREEN, colorama.Fore.YELLOW)
+                output.extend(i
+                              for color, item in zip(itertools.cycle(zebra), run)
+                              for i in (color, self.symbols[item]))
+            output.append(colorama.Style.RESET_ALL)
+            print(''.join(output))
 
 
 class VisitedMap:
-    def __init__(self, side):
-        self.side = abs(int(side))
-        self.map = 0
+    def __init__(self, side: int):
+        self.side = abs(side)
+        self.map = 0  # A very large bitmap
 
-    def __call__(self, n1, n2):
+    def __call__(self, n1: int, n2: int) -> bool:
         pos = n1 * self.side + n2
         mask = 1 << pos
         try:
@@ -48,76 +80,58 @@ class VisitedMap:
         finally:
             self.map |= mask
 
-    def inform(self, that):
-        that.map |= self.map
-
-    def find_pair(self):
-        length = self.side * self.side
-        mask = (1 << length) - 1
-        search = mask ^ self.map
-        if search == 0:
-            return None
+    def iterate_free_pairs(self) -> Iterable[Tuple[int, int]]:
+        mask = (1 << self.side * self.side) - 1
         pos = 0
-        while not search & 1:
-            search >>= 1
-            pos += 1
-        return pos // self.side, pos % self.side
+        search = mask ^ self.map
+        while search:
+            while not search & 1:
+                search >>= 1
+                pos += 1
+            yield pos // self.side, pos % self.side
+            # Assume the map changed, so reload the search bitmap
+            search = (mask ^ self.map) >> pos
 
 
-class ModuloFibonacci:
-    def __init__(self, mod):
-        self.mod = int(mod)
-        if mod <= 0:
-            raise ValueError('Mod must be positive.')
-        self.visited = VisitedMap(self.mod)
-
-    def run(self, n1, n2):
-        visited = VisitedMap(self.mod)
-        while not visited(n1, n2):
-            n1, n2 = n2, (n1 + n2) % self.mod
-            yield n1
-        visited.inform(self.visited)
-
-    def run_through(self):
-        pair = self.visited.find_pair()
-        while pair:
-            assert isinstance(pair, tuple)
-            run = tuple(self.run(*pair))
-            yield pair, run
-            pair = self.visited.find_pair()
+def modulo_fibonacci(base: int) -> Iterable[List[int]]:
+    """ Generates all Modulo Fibonacci runs """
+    if base <= 0:
+        raise ValueError('Mod must be positive.')
+    visited_map = VisitedMap(base)
+    for n1, n2 in visited_map.iterate_free_pairs():
+        run = []
+        while not visited_map(n1, n2):
+            n1, n2 = n2, (n1 + n2) % base
+            run.append(n1)
+        yield run
 
 
-def main(mf):
-    histogram = defaultdict(int)
-    for initial, sequence in mf.run_through():
-        colors(1, 35)
-        numbers(initial[0])
-        colors(36)
-        numbers(*sequence)
-        colors(0)
-        print('')
-        histogram[len(sequence)] += 1
-    print('%d sequences.' % sum(histogram.values()))
-    print('; '.join(('%d of %d' % (amount, length) for (length, amount) in sorted(histogram.items()))))
+def group_by_length(seq: Iterable[list]) -> Dict[int, List[list]]:
+    grouped = collections.defaultdict(list)
+    for item in seq:
+        grouped[len(item)].append(item)
+    # Assuming python 3.7 where all dicts are sorted by insertion order
+    return {k: v for k, v in sorted(grouped.items(), reverse=True)}
 
 
-def colors(*codes):
-    stdout.write('\033[%sm' % ';'.join(map(str, codes)))
-
-
-def numbers(*values):
-    chars = map(lambda x: ALPHABET[x], values)
-    stdout.write(''.join(chars))
-
-
-if __name__ == '__main__':
+def main():
     try:
-        base = int(argv[-1])
+        base = int(sys.argv[-1])
     except ValueError:
         base = 10
     if base <= 0:
         print("Base must be positive.")
-    elif base > len(ALPHABET):
-        print("Settle down! I can't represent more than %d digits." % len(ALPHABET))
-    else:
-        main(ModuloFibonacci(base))
+        return
+
+    # Calculate and dump runs
+    grouped_runs = group_by_length(modulo_fibonacci(base))
+    Alphabet(base).dump(run for group in grouped_runs.values() for run in group)
+
+    # Report some stats
+    histogram = {k: len(v) for k, v in grouped_runs.items()}
+    print(f'{sum(histogram.values())} sequences.')
+    print('; '.join((f'{a} of {l}' for (l, a) in histogram.items())))
+
+
+if __name__ == '__main__':
+    main()
